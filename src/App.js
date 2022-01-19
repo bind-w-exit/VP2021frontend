7ab5d6580ed81e51b721d77cbaa6aadf39bebebf
bind-w-exit <12Andriy24@Gmail.com>
2022-01-19 18:27:50 +0200

    Сompleted the task

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import './App.css';
-import { Button, Modal, Table } from 'react-bootstrap';
+import { Button, Modal, Table, Form } from 'react-bootstrap';
 import { Pencil } from "react-bootstrap-icons";
 import {useState} from 'react';
-import AddProductForm from './components/AddProductForm'
 
 function App() {
-  
-  const [products, setProducts] = useState([]); 
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  const [products, setProducts] = useState([]);
 
   /*  Modal */
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+  const handleClose = () => {setShow(false)};
   const handleShow = () => setShow(true);
 
-  function AddProducts() {
-    setProducts(products.concat([
-      {
-        id: 1,
-        price: '3',
-        name: 'Tree',
-      },
-      {
-        id: 2,
-        price: '10',
-        name: 'Iron',
-      },
-    ]));
+
+  function saveProducts() {
+
+    let product = products.find((item) => item.id === id);
     
-    handleClose();
- }
+    if(product != null) {
+      console.log("edit");
+      product.price = price;
+      product.name = name;
+      handleClose();
+      setName('');
+      setPrice('');
+      setId('');
+    }
+
+    else {
+      console.log("add");
+      setProducts([...products, {id: 'id' + (new Date()).getTime(), name, price}]);
+      handleClose();
+      setName('');
+      setPrice('');
+    }
+  }
+
+  function editProduct(item) {
+    setName(item.name);
+    setPrice(item.price);
+    setId(item.id);
+    handleShow();
+  }
 
   return (
     <div className="App">
@@ -37,15 +53,15 @@ function App() {
         <Table striped bordered hover className='MainTable'>
           <thead>
             <tr>
-              <th width="40%">Price</th>
-              <th width="40%">Name</th>
-              <th width="20%"></th>
+              <th className='PriceColumnHeader'>Price</th>
+              <th className='NameColumnHeader'>Name</th>
+              <th className='EditColumnHeader'></th>
             </tr>
           </thead>
-          <tbody>{products.map((item) => <tr>
-                <td width="40%">{item.price}</td>
-                <td width="40%">{item.name}</td>
-                <td width="20%"><Button><Pencil/></Button></td>   
+          <tbody>{products.map((item) => <tr key={item.id}>
+                <td className='PriceColumn'>{item.price}</td>
+                <td className='NameColumn'>{item.name}</td>
+                <td className='EditColumn'><Button onClick={() => editProduct(item)}><Pencil/></Button></td>   
               </tr>)}
           </tbody>
         </Table>
@@ -61,16 +77,35 @@ function App() {
 
       <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Add product</Modal.Title>
+            <Modal.Title>Product</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <AddProductForm />
+          <Form onSubmit={(event) => {event.preventDefault(); saveProducts(price, name)}}>
+            <Form.Group className='FormPadding'>
+                <Form.Control
+                    type="text"
+                    placeholder="Price *"
+                    required
+                    value={price}
+                    onChange={(event) => setPrice(event.target.value)}
+                    />        
+            </Form.Group>
+            <Form.Group className='FormPadding'>
+                <Form.Control
+                    type="text"
+                    placeholder="Name *"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    required
+                    />        
+            </Form.Group>
+          </Form>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button variant="primary" onClick={AddProducts}>
+            <Button variant="primary" onClick={saveProducts}>
               Save
             </Button>
           </Modal.Footer>
